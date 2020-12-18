93c6995b23c4e1c334a687993c5982a7486975c3
Brian Celenza <bcelenza@gmail.com>
2020-12-18 08:40:58 -0800

    Add pkcs#7 unpad function

--- src/pkcs7.rs
+++ src/pkcs7.rs
@@ -1,3 +1,4 @@
+
 pub mod pkcs7 {
     pub fn pad(input: &Vec<u8>, key_size: &usize) -> Vec<u8> {
         let pad_num: usize;
@@ -9,6 +10,11 @@ pub mod pkcs7 {
             pad_num = *key_size - input.len() % *key_size;
         }
         [input.clone(), vec![pad_num as u8; pad_num]].concat()
+    }
+    
+    pub fn unpad(input: &Vec<u8>) -> Vec<u8> {
+        let pad_num = input.last().unwrap();
+        input[0..input.len() - *pad_num as usize].to_vec()
     }
 
     #[cfg(test)]
@@ -40,6 +46,20 @@ pub mod pkcs7 {
            let result = pad(&input, &key_size);
            let expected = [input, vec![8; 8]].concat();
            assert_eq!(expected, result);
+        }
+
+        #[test]
+        fn test_unpad_single_byte() {
+            let input = vec![0, 0, 0, 1];
+            let result = unpad(&input);
+            assert_eq!(vec![0, 0, 0], result);
+        }
+
+        #[test]
+        fn test_unpad_multi_byte() {
+            let input = vec![0, 0, 0, 4, 4, 4, 4];
+            let result = unpad(&input);
+            assert_eq!(vec![0, 0, 0], result);
         }
     }
 }
